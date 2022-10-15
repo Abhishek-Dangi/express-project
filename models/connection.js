ccb8ebf3e3b1a146e9da9c194ffa700c9cdba545
Abhishek <abhishekdangi2841@gmail.com>
2022-10-16 00:08:28 +0530

    16-10-22

---
+++ models/connection.js
@@ -0,0 +1,8 @@
+// express - mongodb connectivity file for schema implementation
+
+
+import mongoose from 'mongoose';
+
+const url = "mongodb://localhost:27017/abhiproject";
+mongoose.connect(url);
+console.log("Successfully connected to mongodb database......");
